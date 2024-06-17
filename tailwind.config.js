f26a81d54255f2232b6087de0dcf4be8dbc9b637
Edon Souza <e.brendon30@outlook.com>
2024-06-17 01:48:54 -0300

    Style: estilizacao finalizada

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            width: {
+                80: "20rem",
+                100: "24rem",
+            },
+            height: {
+                125: "32rem",
+            },
+            boxShadow: {
+                "card-shadow": "8px 8px 0px 0px rgb(0, 0, 0)",
+            },
+        },
     },
     plugins: [],
 };
